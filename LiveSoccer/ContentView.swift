68fafe1c30ff99b9eb405b92a35a3f451bbba536
yohan <beginnerdevyohan@gmail.com>
2026-03-03 15:10:04 -0500

    Initial Commit

---
+++ LiveSoccer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveSoccer
+//
+//  Created by Yohan Yoon on 3/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveSoccer/LiveSoccerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveSoccerApp.swift
+//  LiveSoccer
+//
+//  Created by Yohan Yoon on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct LiveSoccerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
